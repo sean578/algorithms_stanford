a2dea38737bde425ef1f620f68cfc99d0e71138e
Sean <sean@cortirio.com>
2020-08-09 20:13:40 +0100

    Quicksort - works with any constant pivot index

--- assignment_3.py
+++ assignment_3.py
@@ -1,38 +1,49 @@
-def quicksort(array, comparisons):
+def quicksort(array, comparisons, pivot_index):
     """ Sort data, low to high using quicksort method """
 
     if len(array) <= 1:
         return array, comparisons
 
-    pivot = array[0]
-    array, new_pivot_index = partition(array, 0)
+    pivot = array[pivot_index]
+
+    array = swap_pivot_into_first_index(array, pivot, pivot_index)
+    left_bucket, right_bucket = partition(array)
 
     # recurse left of pivot
-    comparisons += len(array[:new_pivot_index]) - 1
-    array_left, comparisons = quicksort(array[:new_pivot_index],
-                                        comparisons)
+    comparisons += len(left_bucket) - 1
+    ordered_array_left, comparisons = quicksort(left_bucket,
+                                        comparisons,
+                                        pivot_index)
     # recurse right of pivot
-    comparisons += len(array[new_pivot_index+1:]) - 1
-    array_right, comparisons = quicksort(array[new_pivot_index+1:],
-                                             comparisons)
+    comparisons += len(right_bucket) - 1
+    ordered_array_right, comparisons = quicksort(right_bucket,
+                                         comparisons,
+                                         pivot_index)
 
     # concatenate
-    array = array_left + [pivot] + array_right
+    array = ordered_array_left + [pivot] + ordered_array_right
 
     # return the sorted array & the number of comparisons made
     # print('comparison in quicksort', comparisons)
     return array, comparisons
 
 
-def partition(array, pivot_index):
+def swap_pivot_into_first_index(array, pivot, pivot_index):
+    # swap pivot into position 0
+    array[pivot_index] = array[0]
+    array[0] = pivot
+    return array
+
+
+def partition(array):
     """ perform partition around pivot """
 
     # choose the pivot
-    pivot = array[pivot_index]
+    pivot = array[0]
 
     # i, j used to loop through & do comparisons
-    i = pivot_index + 1  # just right of pivot
-    for j in range(pivot_index + 1, len(array)):
+    i = 1  # just right of pivot
+    for j in range(1, len(array)):
         if array[j] < pivot:
             # swap array[i] & array[j]
             temp = array[i]
@@ -44,8 +55,8 @@ def partition(array, pivot_index):
     array[0] = array[i-1]
     array[i-1] = pivot
 
-    # return the ordered array and the pivot index
-    return array, i-1
+    # return two arrays, one each side of pivot
+    return array[:i-1], array[i:]
 
 
 if __name__ == '__main__':
@@ -55,9 +66,10 @@ if __name__ == '__main__':
         data.append(int(line.strip('\n')))
 
     to_show = 5
+    pivot_index = -1
 
     comparisons = 0
-    array, comparisons = quicksort(data, comparisons)
+    array, comparisons = quicksort(data, comparisons, pivot_index)
 
     print('array start: {}\narray finish: {}\n'.format(array[:to_show],
                                                        array[-to_show:]))
